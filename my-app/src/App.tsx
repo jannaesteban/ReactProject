047848d4bc49c12b6b4fdd7882c1dd1d1960d0cf
jannaesteban <esteban@noseryoung.com>
2021-09-23 09:11:29 +0200

    Card added

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -19,6 +21,18 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="https://noseryoung.ch/wp-content/uploads/2020/09/lacs-1.jpg" />
+          <Card.Body>
+            <Card.Title>Shawn Lacarta</Card.Title>
+            <Card.Text>
+              Hello World!
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </body>
     </div>
   );
 }
